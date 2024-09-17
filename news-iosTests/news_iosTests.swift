88b90f9bde5a7b5e7ff7e159ea438091e2290422
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2024-09-17 11:16:53 +0300

    Initial Commit

---
+++ news-iosTests/news_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  news_iosTests.swift
+//  news-iosTests
+//
+//  Created by Mohamed Ibrahim on 17/09/2024.
+//
+
+import Testing
+@testable import news_ios
+
+struct news_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
